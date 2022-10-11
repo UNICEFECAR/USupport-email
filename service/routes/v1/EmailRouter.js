d8a6cc88bdb3e2ce504f28087bb777c40f7c765b
Ivan Ivanov <ivan.h.ivanov99@gmail.com>
2022-10-12 00:46:30 +0100

    refactor

--- service/routes/v1/EmailRouter.js
+++ service/routes/v1/EmailRouter.js
@@ -2,24 +2,26 @@ import express from "express";
 
 import { sendEmail } from "#controllers/email";
 
-import {sendEmailSchema} from "#schemas/EmailControllerSchemas"
+import { sendEmailSchema } from "#schemas/EmailControllerSchemas"
 
 const router = express.Router();
 
-router.post("/", async (req, res, next) => {
-    /**
-     * #route   POST /user/v1/email
-     * #desc    Request to send an email
-     */  
-    const payload = req.body
+router
+    .route("/")
+    .post(async (req, res, next) => {
+        /**
+         * #route   POST /user/v1/email
+         * #desc    Request to send an email
+         */
+        const payload = req.body
 
-    return await sendEmailSchema
-      .noUnknown(true)
-      .strict()
-      .validate(payload)
-      .then(sendEmail) 
-      .then((result) => res.json(result).status(204))
-      .catch(next)
-})
+        return await sendEmailSchema
+            .noUnknown(true)
+            .strict()
+            .validate(payload)
+            .then(sendEmail)
+            .then((result) => res.json(result).status(204))
+            .catch(next)
+    })
 
 export { router };
