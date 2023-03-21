c95575046b7a453aa1e8f66f06e4367596f1e5d6
Georgi Ganchev @7DIGIT <georgi.ganchev@7digit.io>
2023-03-21 18:14:40 +0200

    Add: russian and kazakh translations

---
+++ service/translations/ru.js
@@ -0,0 +1,143 @@
+export default {
+  system_forgot_password_subject: "Забыли пароль",
+  system_forgot_password_title: "Забыли пароль",
+  system_forgot_password_text:
+    "Перейдите по следующей ссылке, чтобы сбросить пароль: {1}",
+
+  system_welcome_subject: "Добро пожаловать в Usupport",
+  system_welcome_title: "Добро пожаловать в Usupport",
+  system_welcome_text:
+    "Перейдите по следующей ссылке, чтобы получить доступ к своему аккаунту: {1}",
+
+  system_login_2fa_request_subject: "Login OTP",
+  system_login_2fa_request_title: "Login 2FA One Time Password",
+  system_login_2fa_request_text:
+    "Please use this code to login into your account: {1}",
+
+  client_consultation_confirm_booking_subject: "Запись на консультацию",
+  client_consultation_confirm_booking_title:
+    "Подтверждение записи на консультацию",
+  client_consultation_confirm_booking_text:
+    "Вы успешно записались на консультацию. Посетите {1}, чтобы просмотреть подробности консультации.",
+
+  client_consultation_confirm_reschedule_subject: "Перенос консультации",
+  client_consultation_confirm_reschedule_title:
+    "Подтверждение переноса консультации",
+  client_consultation_confirm_reschedule_text:
+    "Вы успешно перенесли консультацию. Посетите {1}, чтобы просмотреть новые сведения о консультации.",
+
+  client_consultation_confirm_cancellation_subject: "Отмена консультации",
+  client_consultation_confirm_cancellation_title:
+    "Подтверждение отмены консультации",
+  client_consultation_confirm_cancellation_text:
+    "Вы успешно отменили консультацию. Посетите {1}, чтобы запланировать новую консультацию.",
+
+  client_consultation_notify_cancellation_subject: "Отмена консультации",
+  client_consultation_notify_cancellation_title:
+    "Консультация отменена консультантом",
+  client_consultation_notify_cancellation_text:
+    "Ваша консультация отменена консультантом. Посетите {1}, чтобы запланировать новую консультацию.",
+
+  client_consultation_remind_start_subject: "Напоминание о консультации",
+  client_consultation_remind_start_title: "Напоминание о консультации",
+  client_consultation_remind_start_text:
+    "Ваша консультация начнется через {2} минут(ы). Посетите {1}, чтобы присоединиться к консультации.",
+
+  client_consultation_notify_suggestion_subject: "Предложение консультации",
+  client_consultation_notify_suggestion_title: "Предложение новой консультации",
+  client_consultation_notify_suggestion_text:
+    "У Вас есть предложение о новой консультации. Посетите {1}, чтобы просмотреть дополнительные сведения.",
+
+  client_consultation_confirm_suggestion_booking_subject:
+    "Запись на консультацию",
+  client_consultation_confirm_suggestion_booking_title:
+    "Подтверждение записи на консультацию",
+  client_consultation_confirm_suggestion_booking_text:
+    "Вы успешно приняли предложенную консультацию. Посетите {1}, чтобы просмотреть подробности консультации.",
+
+  client_consultation_confirm_suggestion_cancellation_subject:
+    "Консультация отклонена",
+  client_consultation_confirm_suggestion_cancellation_title:
+    "Подтверждение отказа от консультации",
+  client_consultation_confirm_suggestion_cancellation_text:
+    "You have successfully rejected a suggested consultation. Please visit {1} to schedule a new one.",
+
+  provider_consultation_notify_booking_subject: "Consultation booking",
+  provider_consultation_notify_booking_title: "Запись на новую консультацию",
+  provider_consultation_notify_booking_text:
+    "У Вас новая запись на консультацию. Посетите {1}, чтобы просмотреть дополнительные сведения.",
+
+  provider_consultation_notify_reschedule_subject: "Перенос консультации",
+  provider_consultation_notify_reschedule_title: "Перенос консультации",
+  provider_consultation_notify_reschedule_text:
+    "Консультация перенесена. Посетите {1}, чтобы просмотреть дополнительные сведения.",
+
+  provider_consultation_notify_cancellation_subject:
+    "Отмена консультации",
+  provider_consultation_notify_cancellation_title: "Отмена консультации",
+  provider_consultation_notify_cancellation_text:
+    "Консультация отменена клиентом. Посетите {1}, чтобы просмотреть дополнительные сведения.",
+
+  provider_consultation_confirm_cancellation_subject:
+    "Отмена консультации",
+  provider_consultation_confirm_cancellation_title:
+    "Подтверждение отмены консультации",
+  provider_consultation_confirm_cancellation_text:
+    "Вы успешно отменили консультацию.",
+
+  provider_consultation_remind_start_subject: "Напоминание о консультации",
+  provider_consultation_remind_start_title: "Напоминание о консультации",
+  provider_consultation_remind_start_text:
+    "Ваша консультация начнется через {2} минут(ы). Посетите {1}, чтобы присоединиться к консультации.",
+
+  provider_consultation_confirm_suggestion_subject: "Предложение консультации",
+  provider_consultation_confirm_suggestion_title:
+    "Подтверждение предложенной консультации",
+  provider_consultation_confirm_suggestion_text:
+    "Вы успешно предложили консультацию. Посетите {1}, чтобы просмотреть дополнительные сведения.",
+
+  provider_consultation_notify_suggestion_booking_subject:
+    "Запись на консультацию",
+  provider_consultation_notify_suggestion_booking_title:
+    "Принятое предложение консультации",
+  provider_consultation_notify_suggestion_booking_text:
+    "Ваше предложение консультации было принято. Посетите {1}, чтобы просмотреть дополнительные сведения.",
+
+  provider_consultation_notify_suggestion_cancellation_subject:
+    "Отказ от консультации",
+  provider_consultation_notify_suggestion_cancellation_title:
+    "Отклонено предложение консультации",
+  provider_consultation_notify_suggestion_cancellation_text:
+    "Ваше предложение о консультации отклонено клиентом.",
+
+  provider_availability_remind_add_more_slots_subject: "Напоминание о доступности",
+  provider_availability_remind_add_more_slots_title:
+    "Добавить больше слотов доступности",
+  provider_availability_remind_add_more_slots_text:
+    "Ваш последний слот доступности менее чем через неделю. Посетите {1}, чтобы добавить в свое расписание больше свободных слотов.",
+
+  provider_report_weekly_subject: "Еженедельный отчет",
+  provider_report_weekly_title: "Еженедельный отчет о деятельности",
+  provider_report_weekly_text: "Посетите {1}, чтобы просмотреть еженедельный отчет.",
+
+  provider_report_monthly_subject: "Ежемесячный отчет",
+  provider_report_monthly_title: "Ежемесячный отчет о деятельности",
+  provider_report_monthly_text: "Посетите {1}, чтобы просмотреть ежемесячный отчет.",
+
+  provider_registration_notify_subject: "Регистрация аккаунта",
+  provider_registration_notify_title: "Зарегистрирован как консультант на Usupport",
+  provider_registration_notify_text:
+    "Вы зарегистрированы в качестве консультанта на Usupport.<br/> Ваш автоматически сгенерированный пароль: {2} <br/>Вы можете изменить его в любое время в настройках своего профиля.<br/> Посетите {1}, чтобы войти в свой профиль.",
+  month_1: "January",
+  month_2: "February",
+  month_3: "March",
+  month_4: "April",
+  month_5: "May",
+  month_6: "June",
+  month_7: "July",
+  month_8: "August",
+  month_9: "September",
+  month_10: "October",
+  month_11: "November",
+  month_12: "December",
+};
